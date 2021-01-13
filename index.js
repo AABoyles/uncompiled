2bfd2f4e5d73b844df9160ccdfe57e7951b6be13
Tony Boyles <tony@ephemer.ai>
2021-01-13 13:53:33 -0500

    Spun extension out to showdown-tufte

--- index.js
+++ index.js
@@ -24,31 +24,10 @@
   let post = params.get('q') || 'README.md';
   let main = document.querySelector('article');
 
-  let sidenoteCounter = 0;
-  let sidenoteparser = () => [{
-    type: 'output',
-    regex: /<a href="sidenote">(.*?)<\/a>/g,
-    replace: `<label for="sn-${++sidenoteCounter}" class="margin-toggle sidenote-number"></label><input type="checkbox" id="sn-sn-${sidenoteCounter}" class="margin-toggle"><span class="sidenote">$1</span>`
-  }];
-
-  marginnoteCounter = 0;
-  let marginnoteparser = () => [{
-    type: 'output',
-    regex: /<a href="marginnote">(.*?)<\/a>/g,
-    replace: `<label for="mn-${++marginnoteCounter}" class="margin-toggle"></label><input type="checkbox" id="mn-${marginnoteCounter}" class="margin-toggle"><span class="marginnote">$1</span>`
-  }];
-
-  //Todo: Parse the DOM and do this properly.
-  let sectionwrapper = () => [{
-    type: 'output',
-    regex: /<h2(.*?)>/g,
-    replace: '</section><section><h2$1>'
-  }];
-
   let converter = new showdown.Converter({
     'tables': true, 
     'simplifiedAutoLink': true,
-    'extensions': [sidenoteparser, marginnoteparser, sectionwrapper]
+    'extensions': ['tufte']
   });
 
   let parser = new DOMParser();
